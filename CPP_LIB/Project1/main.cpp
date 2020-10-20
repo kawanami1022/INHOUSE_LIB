a306b3071488353c1065c6ce777f329be57643fc
kawanami1022 <kawanamisos@gmail.com>
2020-10-20 13:35:21 +0900

    added TMX

--- CPP_LIB/Project1/main.cpp
+++ CPP_LIB/Project1/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <fstream>
+#include <string>
 
+using namespace std;
 int main()
 {
-	std::max(3, 1);
+
+	auto filePos = 0;
+	
+	std::fstream FileStream("map.tmx", std::ios_base::in);
+	auto opened = FileStream.is_open();
+	if (!opened)
+	{
+		std::cout << "tmx file open failed!" << std::endl;
+	}
+	else {
+		std::cout << "tmx file open succeed!" << std::endl;
+	}
+
+	if (FileStream.eof())
+	{
+		
+	}
+	
 }
 
---
+++ CPP_LIB/Project1/main.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <memory>
+
+struct TMX_FileSize
+{
+	
+};
+
+
+class TMX
+{
+	
+};
