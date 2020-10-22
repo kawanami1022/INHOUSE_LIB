04922a13c09a69a8d31ba1f9b7d64e693048bb3f
kawanami1022 <kawanamisos@gmail.com>
2020-10-22 14:20:52 +0900

    Implemention Completed

---
+++ CPP_LIB/Project1/strmanip/strmanip.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <unordered_map>
+#include <memory>
+#include <string>
+namespace strmanip
+{
+	std::string ExtractTheStrDblQt(std::string lineString, std::string stringId);
+}
